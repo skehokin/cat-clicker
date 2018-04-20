f4dc7e3aaefcca3e069db11e806428f38ce7d7aa
Siobhan Hokin <skehokin@gmail.com>
2018-04-20 14:59:55 +1000

    Add onclick setTimeout for all frames.

--- cat.js
+++ cat.js
@@ -3,8 +3,21 @@ function listrand(listname) {
 }
 
 var cat = document.getElementsByClassName("cat-image")[0];
-console.log(cat);
+
+var resting = []
+var active = []
+
+for (i=1; i<5; i++){
+	resting.push("Frame"+i)
+}
+
+for (i=5; i<13; i++){
+	active.push("Frame"+i)
+}
 
 cat.onclick = function() {
-	this.innerHTML = '<img src="images/Frame5.png" alt="a pink cat head">';
+	cat.innerHTML = '<img src="images/'+listrand(active)+'.png" alt="a pink cat head">';
+	setTimeout(function(){
+		cat.innerHTML = '<img src="images/'+listrand(resting)+'.png" alt="a pink cat head">';
+	}, 500);
 }
