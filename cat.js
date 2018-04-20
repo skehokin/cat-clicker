351da05ba08fb6ed07887e0204107980c8f55c09
Siobhan Hokin <skehokin@gmail.com>
2018-04-20 15:49:07 +1000

    implement click count

--- cat.js
+++ cat.js
@@ -5,6 +5,7 @@ function listrand(listname) {
 var cat = document.getElementsByClassName("cat-image")[0];
 var catRest = document.getElementsByClassName("lower")[0];
 var catAct = document.getElementsByClassName("upper")[0];
+var number = document.getElementsByClassName("number")[0];
 
 var resting = []
 var active = []
@@ -16,12 +17,15 @@ for (i=1; i<5; i++){
 for (i=1; i<13; i++){
 	active.push("Frame"+i)
 }
-
+count = 0
 cat.onclick = function() {
 	catAct.innerHTML = '<img src="images/'+listrand(active)+'.png" alt="a pink cat head">';
+	count++;
 	setTimeout(function(){
-		catRest = '<img src="images/'+listrand(resting)+'.png" alt="a pink cat head">';
+		catRest.innerHTML = '<img src="images/'+listrand(resting)+'.png" alt="a pink cat head">';
+		number.innerHTML = count;
+	}, 100);
+	setTimeout(function(){
 		catAct.innerHTML = '';
-
 	}, 700);
 }
