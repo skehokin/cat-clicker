26b9aaa6d8f2959c88befb6b05bbcd4e0b5aea8e
Siobhan Hokin <skehokin@gmail.com>
2018-04-20 14:50:06 +1000

    Onclick function for cat image.

--- cat.js
+++ cat.js
@@ -1,3 +1,10 @@
 function listrand(listname) {
 	return listname[Math.floor(Math.random()*listname.length)]
 }
+
+var cat = document.getElementsByClassName("cat-image")[0];
+console.log(cat);
+
+cat.onclick = function() {
+	this.innerHTML = '<img src="images/Frame5.png" alt="a pink cat head">';
+}
